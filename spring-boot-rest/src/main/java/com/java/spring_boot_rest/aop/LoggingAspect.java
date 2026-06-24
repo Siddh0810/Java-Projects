6581d7349fc1d1daafe5331e40f79c3636e4e950
Siddh0810 <shahsiddh08@gmail.com>
2026-06-24 19:12:40 +0530

    Spring AOP - After Advice.

--- spring-boot-rest/src/main/java/com/java/spring_boot_rest/aop/LoggingAspect.java
+++ spring-boot-rest/src/main/java/com/java/spring_boot_rest/aop/LoggingAspect.java
@@ -1,9 +1,7 @@
 package com.java.spring_boot_rest.aop;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -15,9 +13,24 @@ public class LoggingAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
 //    return-type class-name.method-name(arguments)
-    @Before("execution(* com.java.spring_boot_rest.service.*.*(..))")
+    @Before("execution(* com.java.spring_boot_rest.service.JobService.*(..))")
     public void logMethodCall(JoinPoint jp){
         LOGGER.info("Method called: " + jp.getSignature().getName());
+    }
+
+    @After("execution(* com.java.spring_boot_rest.service.JobService.getJobs(..)) || execution(* com.java.spring_boot_rest.service.JobService.saveJobPost(..))")
+    public void logMethodExecuted(JoinPoint jp){
+        LOGGER.info("Method executed: " + jp.getSignature().getName());
+    }
+
+    @AfterThrowing("execution(* com.java.spring_boot_rest.service.JobService.*(..))")
+    public void logMethodExecutedAfterException(JoinPoint jp){
+        LOGGER.info("Method has some problem: " + jp.getSignature().getName());
+    }
+
+    @AfterReturning("execution(* com.java.spring_boot_rest.service.JobService.*(..))")
+    public void logMethodExecutedOnSuccess(JoinPoint jp){
+        LOGGER.info("Method ran successfully: " + jp.getSignature().getName());
     }
 
 }
--- spring-boot-rest/src/main/java/com/java/spring_boot_rest/model/JobPost.java
+++ spring-boot-rest/src/main/java/com/java/spring_boot_rest/model/JobPost.java
@@ -26,65 +26,8 @@ public class JobPost {
     @Lob
     private byte[] imageData;
 
-
-    public String getImageName() {
-        return imageName;
-    }
-
-    public void setImageName(String imageName) {
-        this.imageName = imageName;
-    }
-
-    public String getImageType() {
-        return imageType;
-    }
-
-    public void setImageType(String imageType) {
-        this.imageType = imageType;
-    }
-
-    public byte[] getImageData() {
-        return imageData;
-    }
-
-    public void setImageData(byte[] imageData) {
-        this.imageData = imageData;
-    }
-
     public JobPost(int id) {
         this.postId = id;
-    }
-
-    public int getPostId() {
-        return postId;
-    }
-
-    public void setPostId(int postId) {
-        this.postId = postId;
-    }
-
-    public String getPostProfile() {
-        return postProfile;
-    }
-
-    public void setPostProfile(String postProfile) {
-        this.postProfile = postProfile;
-    }
-
-    public String getPostDesc() {
-        return postDesc;
-    }
-
-    public void setPostDesc(String postDesc) {
-        this.postDesc = postDesc;
-    }
-
-    public int getReqExperience() {
-        return reqExperience;
-    }
-
-    public void setReqExperience(int reqExperience) {
-        this.reqExperience = reqExperience;
     }
 
 }
